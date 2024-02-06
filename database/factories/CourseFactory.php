552a1e1ddccbde6df3dfaa01900e168d23a8507c
michel-flexcode <michelseocode@gmail.com>
2024-02-06 14:26:04 +0100

    course code added

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -18,6 +18,8 @@ class CourseFactory extends Factory
     {
         return [
             'name' => $this->faker->word(10),
+            'code' => $this->faker->numerify('#### ## U## D#'),
+           
         ];
     }
 }
