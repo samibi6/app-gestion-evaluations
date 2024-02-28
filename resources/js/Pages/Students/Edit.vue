52731eddea3040569ce12d071b1c35033beff7dc
Aure6 <aurelien7112001@gmail.com>
2024-02-28 21:37:56 +0100

    Update submit button text from "Soumettre" to "Sauvegarder"

--- resources/js/Pages/Students/Edit.vue
+++ resources/js/Pages/Students/Edit.vue
@@ -102,7 +102,7 @@ var closeModal = () => {
                         <div class="flex justify-center">
                             <button :disabled="form.processing"
                                 class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 block">
-                                Soumettre
+                                Sauvegarder
                             </button>
                             <button type="button"
                                 class="rounded-lg px-5 py-2.5 me-2 mb-2 block font-medium text-red-500 hover:bg-red-200"
