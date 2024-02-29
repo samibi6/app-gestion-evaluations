ec7b479c9afea89983816d7d77e0f162e383f03a
Aure6 <aurelien7112001@gmail.com>
2024-02-29 19:44:20 +0100

    Update student edit page with new button styles

--- resources/js/Pages/Students/Edit.vue
+++ resources/js/Pages/Students/Edit.vue
@@ -100,14 +100,20 @@ var closeModal = () => {
                             </div>
                         </div>
 
-                        <div class="flex justify-center">
+                        <div class="flex justify-center gap-2">
                             <button :disabled="form.processing"
-                                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 block">
+                                class="text-white bg-blue-900 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                 Sauvegarder
                             </button>
                             <button type="button"
-                                class="rounded-lg px-5 py-2.5 me-2 mb-2 block font-medium text-red-500 hover:bg-red-200 text-sm"
+                                class="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                                 @click="confirmStudentDeletion(student.id)">
+                                <svg class="w-5 h-5 mr-1 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
                                 Supprimer l'étudiant
                             </button>
                         </div>
