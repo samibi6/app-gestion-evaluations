ee76da8b62c2fc320b0c17ea75231e2211cb9887
samibi6 <sambiname@yahoo.fr>
2024-03-12 07:31:01 +0100

    style and pdf views fixes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -20,13 +20,15 @@ import { AcademicCapIcon } from '@heroicons/vue/24/solid';
 
                         <AcademicCapIcon class="block h-16 w-auto text-blue-600" />
 
-                        <h1 class="ms-4 font-thin text-3xl"><span class="font-black">App</span> Gestion
+                        <h1 class="ms-4 font-thin text-3xl text-gray-500"><span class="font-black text-black">App</span>
+                            Gestion
                             Evaluations
                         </h1>
                     </div>
                     <div class="grid grid-cols-3 gap-5 justify-center items-center">
 
-                        <a class="hover:scale-105 origin-center transition block h-full" :href="route('sections.index')">
+                        <a class="hover:scale-105 origin-center transition block h-full"
+                            :href="route('sections.index')">
                             <div
                                 class="hover:shadow-lg hover:bg-indigo-100 hover:border-blue-600 transition rounded-md border-2 p-5 bg-gray-200 h-full">
                                 <h2 class="text-xl mb-1 font-bold text-blue-600">Sections</h2>
@@ -50,7 +52,8 @@ import { AcademicCapIcon } from '@heroicons/vue/24/solid';
                                 <p class="text-gray-600 text-lg">Assigner / Retirer <br>cours de section</p>
                             </div>
                         </a>
-                        <a class="hover:scale-105 origin-center transition block h-full" :href="route('students.index')">
+                        <a class="hover:scale-105 origin-center transition block h-full"
+                            :href="route('students.index')">
                             <div
                                 class="hover:shadow-lg hover:bg-indigo-100 hover:border-blue-600 transition rounded-md border-2 p-5 bg-gray-200 h-full">
                                 <h2 class="text-xl mb-1 font-bold text-blue-600">Etudiants</h2>
@@ -74,7 +77,8 @@ import { AcademicCapIcon } from '@heroicons/vue/24/solid';
 
 
 
-                        <a class="hover:scale-105 origin-center transition block h-full" :href="route('aptitudes.index')">
+                        <a class="hover:scale-105 origin-center transition block h-full"
+                            :href="route('aptitudes.index')">
                             <div
                                 class="hover:shadow-lg hover:bg-indigo-100 hover:border-blue-600 transition rounded-md border-2 p-5 bg-gray-200 h-full">
                                 <h2 class="text-xl mb-1 font-bold text-blue-600">AA et critères</h2>
@@ -88,7 +92,6 @@ import { AcademicCapIcon } from '@heroicons/vue/24/solid';
                         <a class="hover:scale-105 origin-center transition block col-span-1 h-full"
                             :href="route('invites.index')">
                             <div
-
                                 class="hover:shadow-lg hover:bg-indigo-100 hover:border-blue-600 transition rounded-md border-2 p-5 bg-gray-200 h-full">
                                 <h2 class="text-xl mb-1 font-bold text-blue-600">Invitations</h2>
                                 <p class="text-gray-600 text-lg">Générer un jeton pour inviter un nouvel utilisateur</p>
@@ -99,7 +102,6 @@ import { AcademicCapIcon } from '@heroicons/vue/24/solid';
                         <a class="hover:scale-105 origin-center transition block col-span-1 h-full"
                             :href="route('evals.index')">
                             <div
-
                                 class="hover:shadow-lg hover:bg-indigo-100 hover:border-blue-600 transition rounded-md border-2 p-5 bg-gray-200 h-full">
                                 <h2 class="text-xl mb-1 font-bold text-blue-600">Evaluations</h2>
 
